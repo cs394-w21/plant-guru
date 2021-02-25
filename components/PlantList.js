aedb41d7bcebccf44f790fbaaa4b20fd632f97a7
ptarasul18 <paulina.tarasul@gmail.com>
2021-02-24 21:12:21 -0600

    last filtering functions

--- components/PlantList.js
+++ components/PlantList.js
@@ -42,8 +42,73 @@ const PlantList = (props) => {
         }
     };
 
+    const checkEffort = plant => {
+        const currEffortLowerCase = plant.care.toLowerCase();
+        const effortLowerCase = effort.toLowerCase();
+        if (effortLowerCase === 'easy' && currEffortLowerCase.includes('low')) {
+            return true;
+        } else if (currEffortLowerCase.includes(effortLowerCase)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    const checkSize = plant => {
+        const currSizeLowerCase = plant.size.toLowerCase();
+        const sizeLowerCase = size.toLowerCase();
+        if(currSizeLowerCase.includes("feet")){
+            let splitSize = currSizeLowerCase.split("feet");
+            let splitH = splitSize[0].slice(-1).split("-");
+            let maxHeight = splitH[1]
+            if (sizeLowerCase === 'small' && Number(maxHeight)<6) {
+                return true;
+            } else if (sizeLowerCase == "medium" && (6 <= Number(maxHeight) < 15)) {
+                return true;
+            } else if (sizeLowerCase === 'large' && Number(maxHeight)>=15) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        else if (currSizeLowerCase.includes("inches") && sizeLowerCase=="small"){
+            return true;
+        }
+        else{
+            return false;
+        }
+        
+    }
+    const checkToxic = plant => {
+        const toxic = plant.allergies.toLowerCase();
+        const children1 = children.toLowerCase();
+        const pets1 = pets.toLowerCase();
+        if (toxic == "none"){
+            return true;
+        }
+        else if (children1 == "yes" || pets1=="yes"){
+            if (toxic.includes("poisonous")){
+                return false;
+            }
+            else if (children1 == "yes" && toxic.includes("children")){
+                return false;
+            }
+            else if (pets1 == "yes" && toxic.includes("pets")){
+                return false;
+            }
+            else{
+                return true;
+            }
+        }
+        else{
+            return true;
+        }
+
+    }
+
     const filterPlants = plant => {
-        return checkSunlight(plant) && checkTemperature(plant) && checkHumidity(plant);
+        return checkSunlight(plant) && checkTemperature(plant) && checkHumidity(plant) && checkEffort(plant) && checkSize(plant) && checkToxic(plant);
     };
 
     const filteredPlants = plants.filter(filterPlants);
